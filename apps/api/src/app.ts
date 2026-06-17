9e9e69eaafc7a0f71cffa6fa00e88c806d81947d
tiesen243 <ttien56906@gmail.com>
2026-06-17 23:15:24 +0700

    chore: minor changes

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,3 @@
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
 import { Elysia } from 'elysia'
 
 import { CreatePostUseCase } from '@/application/use-cases/post/create.use-case'
@@ -7,12 +6,7 @@ import { OnePostUseCase } from '@/application/use-cases/post/one.use-case'
 import { UpdatePostUseCase } from '@/application/use-cases/post/update.use-case'
 import { PostRepositoryMemory } from '@/infras/memory/post.repository.menory'
 import { postRoute } from '@/interfaces/http/post.route'
-import { postTRPC } from '@/interfaces/rpc/post.trpc'
-import { ApiResponse } from '@/shared/api-response'
 import { loggerPlugin } from '@/shared/plugins/logger.plugin'
-import { createTRPCRouter } from '@/shared/trpc'
-
-import packageJson from '../package.json' assert { type: 'json' }
 
 export async function createApp() {
   const postRepo = new PostRepositoryMemory()
@@ -24,36 +18,14 @@ export async function createApp() {
     update: new UpdatePostUseCase(postRepo),
   }
 
-  const routes = new Elysia({
-    name: 'root.routes',
-  })
+  const app = new Elysia()
+    // Register plugins
+    .use(loggerPlugin)
 
-    .get('/', () =>
-      ApiResponse.ok('Welcome to the API!', {
-        name: packageJson.name,
-        version: packageJson.version,
-      })
-    )
-
+    // Register HTTP routes
     .use(postRoute(postUseCases))
 
-  const rpc = new Elysia({
-    name: 'root.rpc',
-  }).all('/rpc/*', ({ request }) =>
-    fetchRequestHandler({
-      endpoint: '/rpc',
-      req: request,
-      router: createTRPCRouter({
-        post: postTRPC(postUseCases),
-      }),
-    })
-  )
-
-  return new Elysia({ name: 'root' })
-    .use(loggerPlugin)
-    .use(routes)
-    .use(rpc)
-    .compile()
+  return app.compile()
 }
 
 export type App = Awaited<ReturnType<typeof createApp>>
--- apps/api/src/shared/trpc.ts
+++
@@ -1,9 +0,0 @@
-import { initTRPC } from '@trpc/server'
-
-const t = initTRPC.create()
-
-const createTRPCRouter = t.router
-
-const publicProcedure = t.procedure
-
-export { createTRPCRouter, publicProcedure }
